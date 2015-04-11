577b0b618fdd79f19084f578c3e7706d523118bf
Josh Stickles <sticklesjb@gmail.com>
2015-04-11 14:09:01 -0400

    more work during the hackathon

---
+++ public/js/app.js
@@ -0,0 +1,46 @@
+(function($){
+	var Champ = Backbone.Model.extend({});
+
+	var Champs =  Backbone.Collection.extend({
+		model: Champ,
+		parse: function(response) {
+			return response.champions;
+		},
+		url: "https://na.api.pvp.net/api/lol/na/v1.2/champion?api_key=e502b879-b576-4bbc-bc52-0f1f04ce7ee1"
+	});
+
+	var allChamps = new Champs();
+
+	var selectedChamp = null;
+
+	allChamps.fetch({
+		success: function(collection, response, options){
+			selectedChamp = collection.models[0];
+			_.each(collection.models, function(champ){
+				$.ajax({
+					url: "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/"+champ.get("id")+"?champData=all&api_key=e502b879-b576-4bbc-bc52-0f1f04ce7ee1",
+					success: function(response) {
+						$(".champion-list").append("<li class='champion'><a data-id='"+champ.get("id")+"' href='#champ'><img src='http://ddragon.leagueoflegends.com/cdn/5.7.2/img/champion/"+response.image.full+"'/><p>"+response.name+"</p></a></li>");
+					}, 
+					error: function(response) {
+						console.log("error fetching individual details");
+					}
+				});
+			});
+			$(".champion-list").listview();	
+		},
+		error: function(collection, response, options){
+			console.log("error");
+		}
+	});
+
+	$("body").on("click", ".champion", function(event) {
+		selectedChamp = _.find(allChamps, function(champ){
+			return $(event.target).data("id") == champ.id;
+		});
+	});
+
+	$(document).on( "pagebeforeshow", "#champ", function( event ) {
+		console.log(selectedChamp.get("id"));
+	});
+})(jQuery);
